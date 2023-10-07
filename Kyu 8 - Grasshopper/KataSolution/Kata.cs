889463f37cf7357377b2f9726d9ba26d6e71c971
Flemming Madsen <f.m.madsen@live.dk>
2023-10-07 21:55:04 +0200

    Grasshopper (8 kyu) implemented

---
+++ Kyu 8 - Grasshopper/KataSolution/Kata.cs
@@ -0,0 +1,17 @@
+﻿using KataSolution.Game;
+
+namespace KataSolution
+{
+    public class Kata
+    {
+        public static void DoTurn()
+        {
+            Logic.RollDice();
+            Logic.Move();
+            Logic.Combat();
+            Logic.GetCoins();
+            Logic.BuyHealth();
+            Logic.PrintStatus();
+        }
+    }
+}
---
+++ Kyu 8 - Grasshopper/TestProject/SolutionTest.cs
@@ -0,0 +1,23 @@
+﻿using KataSolution;
+using KataSolution.Game;
+
+namespace TestProject
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test, Description("One Turn")]
+        public void Test()
+        {
+            Kata.DoTurn();
+
+            Assert.AreEqual(6, Logic.Log.Count, "should call all 6 functions");
+            Assert.AreEqual("RollDice", Logic.Log[0], "should roll dice first");
+            Assert.AreEqual("Move", Logic.Log[1], "should move second");
+            Assert.AreEqual("Combat", Logic.Log[2], "should combat third");
+            Assert.AreEqual("GetCoins", Logic.Log[3], "should get coins fourth");
+            Assert.AreEqual("BuyHealth", Logic.Log[4], "should buy health fifth");
+            Assert.AreEqual("PrintStatus", Logic.Log[5], "should print status sixth");
+        }
+    }
+}
