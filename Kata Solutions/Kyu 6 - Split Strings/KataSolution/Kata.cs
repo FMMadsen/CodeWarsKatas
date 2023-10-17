4e296e6f5e07e43f3e15912ed103fef4d084206b
Flemming Madsen <f.m.madsen@live.dk>
2023-10-17 23:39:18 +0200

    Fixed the Split Strings kata

--- Kata Solutions/Kyu 6 - Split Strings/KataSolution/Kata.cs
+++ Kata Solutions/Kyu 6 - Split Strings/KataSolution/Kata.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KataSolution
 {
-    internal class Kata
+    public static class Kata
     {
+        public static string[] SplitString(string input)
+        {
+            List<string> result = new List<string>();
+
+            if (!string.IsNullOrEmpty(input))
+            {
+                if(input.Length % 2 == 1)
+                {
+                    input += "_";
+                }
+
+                int loops = input.Length / 2;
+                
+                for(int i = 0; i < loops; i++)
+                {
+                    result.Add(input.Substring(i * 2,2));
+                }
+            }
+
+            return result.ToArray();
+        }
     }
 }
--- Kata Solutions/Kyu 6 - Split Strings/TestProject/TestKata.cs
+++ Kata Solutions/Kyu 6 - Split Strings/TestProject/TestKata.cs
@@ -3,10 +3,19 @@ namespace TestProject
     [TestFixture]
     public class TestKata
     {
-        [Test]
-        public void Test_Kyu_6_SplitStrings()
+        [TestCase("", new string[] { })]
+        [TestCase("a", new string[] { "a_" })]
+        [TestCase("abc", new string[] { "ab", "c_" })]
+        [TestCase("abcdef", new string[] { "ab", "cd", "ef" })]
+        public void Test_Kyu_6_SplitStrings(string input, string[] expectedOutput)
         {
-            Assert.Pass();
+            // Arrange
+
+            // Act
+            var actual = KataSolution.Kata.SplitString(input);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expectedOutput));
         }
     }
 }
