4e3ca098c72af24a33f863227974446b60c65cc3
Flemming Madsen <f.m.madsen@live.dk>
2023-10-26 01:08:05 +0200

    Solved 5 Kyu: Maximum subarray sum

--- Kata Solutions/Kyu 5 - Maximum subarray sum/KataSolution/Kata.cs
+++ Kata Solutions/Kyu 5 - Maximum subarray sum/KataSolution/Kata.cs
@@ -1,16 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KataSolution
+﻿namespace KataSolution
 {
     public static class Kata
     {
-        public static int MaxSequence(int[] intArrayInput)
+        /// <summary>
+        /// 
+        /// { -2, 1, -3, 4, -1, 2, 1, -5, 4 }   => 6
+        /// { -2, -1, 0, 1, 2, 3, 4, 5 }        => 15
+        /// { 2, 3, 4 }                         => 9
+        /// { 2, 3, 4, -3, 2 }                  => 9
+        /// { 0 }                               => 0
+        /// 
+        /// </summary>
+        /// <param name="intArray">input aray if int</param>
+        /// <returns>Largest sum of consequitive numbers</returns>
+        public static int MaxSequence(int[] intArray)
         {
-            return 0;
+            int sumMax = 0;
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                int sum = DetermineLargestSumInSequence(intArray, i);
+                sumMax = Max(sum, sumMax);
+            }
+
+            return sumMax;
+        }
+
+        public static int DetermineLargestSumInSequence(int[] intArray, int startIndex)
+        {
+            if (intArray.Length == 0)
+                return 0;
+
+            if (startIndex >= intArray.Length)
+                return 0;
+
+            if (intArray[startIndex] < 1)
+                return 0;
+
+            int sumMax = 0;
+            int tmpSum = 0;
+
+            for (int i = startIndex; i < intArray.Length; i++)
+            {
+                tmpSum += intArray[i];
+                sumMax = Max(tmpSum, sumMax);
+            }
+
+            return sumMax;
+        }
+
+        private static int Max(int a, int b)
+        {
+            return a > b ? a : b;
         }
     }
 }
--- Kata Solutions/Kyu 5 - Maximum subarray sum/TestProject/TestKata.cs
+++ Kata Solutions/Kyu 5 - Maximum subarray sum/TestProject/TestKata.cs
@@ -6,7 +6,11 @@ namespace TestProject
     public class TestKata
     {
         [TestCase(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }, 6)]
+        [TestCase(new int[] { -2, -1, 0, 1, 2, 3, 4, 5 }, 15)]
+        [TestCase(new int[] { 2, 3, 4 }, 9)]
+        [TestCase(new int[] { 2, 3, 4, -3, 2 }, 9)]
         [TestCase(new int[] { 0 }, 0)]
+        [TestCase(new int[] { }, 0)]
         public void Test_Kyu_5_MaxSequence(int[] input, int expectedOutput)
         {
             // Arrange
