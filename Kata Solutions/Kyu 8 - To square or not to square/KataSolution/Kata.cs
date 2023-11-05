5ffdba70ecfcccbf2ef7e89e7bba4ab55e67b447
Flemming Madsen <f.m.madsen@live.dk>
2023-11-05 23:40:53 +0100

    Solved new Kata. 8th Kyu. To square(root) or not to square(root).

---
+++ Kata Solutions/Kyu 8 - To square or not to square/KataSolution/Kata.cs
@@ -0,0 +1,28 @@
+﻿namespace KataSolution
+{
+    public class Kata
+    {
+        public static int[] SquareOrSquareRoot(int[] array)
+        {
+            var newArray = new int[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = SquareOrSquareRoot(array[i]);
+            }
+
+            return newArray;
+        }
+
+        public static int SquareOrSquareRoot(int number)
+        {
+            double squareRoot = System.Math.Sqrt(number);
+            int squareRootIntegeer = (int)squareRoot;
+
+            if (squareRoot == squareRootIntegeer)
+                return squareRootIntegeer;
+            else
+                return number * number;
+        }
+    }
+}
---
+++ Kata Solutions/Kyu 8 - To square or not to square/TestProject/TestKata.cs
@@ -0,0 +1,19 @@
+namespace TestProject
+{
+    public class TestKata
+    {
+        [TestCase(new int[] { 4, 3, 9, 7, 2, 1 }, new int[] { 2, 9, 3, 49, 4, 1 })]
+        [TestCase(new int[] { 100, 101, 5, 5, 1, 1 }, new int[] { 10, 10201, 25, 25, 1, 1 })]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6 }, new int[] { 1, 4, 9, 2, 25, 36 })]
+        public void Test_Kyu_6_SplitStrings(int[] input, int[] expectedOutput)
+        {
+            // Arrange
+
+            // Act
+            var actual = KataSolution.Kata.SquareOrSquareRoot(input);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expectedOutput));
+        }
+    }
+}
